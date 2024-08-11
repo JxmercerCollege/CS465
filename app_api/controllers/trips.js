32863501b55a9aca6f4c4570d0102b83165123d2
Joshua <mudslinger_kid@yahoo.com>
2024-08-11 09:12:42 -0500

    This week broke me. Signed-off-by: Joshua <mudslinger_kid@yahoo.com>

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -17,7 +17,7 @@ const tripsList = async(req, res) => {
             .json(q);
     }
 };
-
+//GET REQEST to get trip by code
 const tripsFindByCode = async(req, res) =>{
     console.log("Received tripCode:", req.params.tripCode);
     const q = await Model
@@ -36,8 +36,78 @@ const tripsFindByCode = async(req, res) =>{
             .json(q);
     }
 };
+//POST request to add a trip
+const tripsAddTrip = async (req, res) => {
+    const newTrip = new Trip({
+        code: req.body.code,
+        name: req.body.name,
+        length: req.body.length,
+        start: req.body.start,
+        resort: req.body.resort,
+        perPerson: req.body.perPerson,
+        image: req.body.image,
+        description: req.body.description
+    });
+
+    try {
+        const savedTrip = await newTrip.save();
+        return res
+            .status(201)
+            .json(savedTrip);
+    } catch (err) {
+        return res
+            .status(400)
+            .json({ message: 'Failed to add trip', error: err });
+    }
+};
+const tripsUpdateTrip = async (req, res) => {
+    // Uncomment for debugging
+    console.log(req.params);
+    console.log(req.body);
+
+    try {
+        const q = await Model
+            .findOneAndUpdate(
+                { 'code': req.params.tripCode }, // Find the trip by its code
+                {
+                    code: req.body.code,
+                    name: req.body.name,
+                    length: req.body.length,
+                    start: req.body.start,
+                    resort: req.body.resort,
+                    perPerson: req.body.perPerson,
+                    image: req.body.image,
+                    description: req.body.description
+                }, 
+                { new: true } // Return the updated document
+            )
+            .exec();
+
+        if (!q) {
+            // Database returned no data
+            return res
+                .status(400)
+                .json({ message: 'Trip not found' });
+        } else {
+            // Return the resulting updated trip
+            return res
+                .status(200) // 201 is typically used for resource creation, 200 is more appropriate for updates
+                .json(q);
+        }
+    } catch (err) {
+        return res
+            .status(400)
+            .json({ message: 'Failed to update trip', error: err });
+    }
+
+    // Uncomment the following line to show results of operation
+    // on the console
+    // console.log(q);
+};
 
 module.exports = {
     tripsList,
-    tripsFindByCode
+    tripsFindByCode,
+    tripsAddTrip,
+    tripsUpdateTrip
 }
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -2,11 +2,16 @@ const express = require('express');
 const router = express.Router();
 const tripsController = require('../controllers/trips');
 
+//These are trips enpoints
 router
     .route('/trips') 
-    .get(tripsController.tripsList);
+    .get(tripsController.tripsList)
+    .post(tripsController.tripsAddTrip);
+
+    //These are out REST methods
 router
     .route('/trips/:tripCode')
-    .get(tripsController.tripsFindByCode);
+    .get(tripsController.tripsFindByCode)
+    .put(tripsController.tripsUpdateTrip);
 
 module.exports = router;
